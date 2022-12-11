deef8325f7ab7dff2e6626674f3ae809b1238d93
zpl <1639287627@qq.com>
2022-12-11 20:41:56 +0800

    1：项目初始化

---
+++ src/main/java/com/zpl/dynameic/DynameicDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zpl.dynameic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynameicDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynameicDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zpl/dynameic/DynameicDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zpl.dynameic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynameicDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
